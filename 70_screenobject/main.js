eace4fe071301d2fe89b76671e06b14ca59dc493
Asghar Ali Sindhu <zainmathinter@gmail.com>
2024-01-07 21:31:03 +0500

    70_screenobject

---
+++ 70_screenobject/main.js
@@ -0,0 +1,12 @@
+// todo: The screen object contains information about the visitor's screen.
+
+// console.log(screen.availHeight);
+// console.log(screen.availWidth);
+// console.log(screen.colorDepth);
+// console.log(screen.height);
+// console.log(screen.orientation);
+// console.log(screen.pixelDepth);
+// console.log(screen.width);
+// console.log(screen.console);
+// console.log(screen.log);
+// console.log(screen.screen);
