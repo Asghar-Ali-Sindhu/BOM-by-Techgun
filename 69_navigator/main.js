7de17ab79e0ce6a5a0ae646cd34aac0ffba8e4a0
Asghar Ali Sindhu <zainmathinter@gmail.com>
2024-01-07 21:15:17 +0500

    69-navigator

---
+++ 69_navigator/main.js
@@ -0,0 +1,27 @@
+// todo: The navigator object contains information about the browser of the user. The navigator object is a property of the window object. The navigator object is accessed with: window.navigator or just navigator:
+// let url = window.navigator.language;
+// let url1 = window.navigator.clipboard;
+// let url2 = window.navigator.credentials;
+// let url3 = window.navigator.languages;
+// let url4 = window.navigator.locks;
+// let url5 = window.navigator.geolocation;
+// let url6 = window.navigator.appName;
+// let url7 = window.navigator.appVersion;
+// let url8 = window.navigator.appCodeName;
+// let url9 = window.navigator.cookieEnabled;
+// let url10 = window.navigator.userAgent;
+// let url11 = window.navigator.platform;
+// let url12 = window.navigator.javaEnabled();
+// console.log(url);
+// console.log(url1);
+// console.log(url2);
+// console.log(url3);
+// console.log(url4);
+// console.log(url5);
+// console.log(url6);
+// console.log(url7);
+// console.log(url8);
+// console.log(url9);
+// console.log(url10);
+// console.log(url11);
+// console.log(url12);
